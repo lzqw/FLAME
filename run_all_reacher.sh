999fbed9cb84d6c5f6e7ffc5ca8ce7fec860bbaa
lzqw <lizeqiaoemail@163.com>
2025-10-10 16:09:23 +0800

    how to init log_alpha

---
+++ run_all_reacher.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 如果任何命令失败，脚本将立即退出
+set -e
+
+# --- 脚本开始 ---
+echo "=================================================="
+echo "开始执行 18 个任务 (环境版本: v5)..."
+echo "=================================================="
+echo ""
+
+# --- 指令列表 ---
+
+
+#echo "[Task 1/18] Now running: python scripts/train_mujoco.py --env Hopper-v5 --diffusion_steps 1 --alg sac"
+#python scripts/train_mujoco.py --env Reacher-v5 --diffusion_steps 20 --alg rf_sac_estient  --noise_scale 0.001 --target_entropy_scale 1.0 --alpha_lr 0.007
+#echo "✅ Task 1/18 finished."
+#echo ""
+
+echo "[Task 1/18] Now running: python scripts/train_mujoco.py --env Hopper-v5 --diffusion_steps 1 --alg sac"
+python scripts/train_mujoco.py --env Reacher-v5 --diffusion_steps 20 --alg rf_sac_estient  --noise_scale 0.1 --target_entropy_scale 1.0 --alpha_lr 0.001
+echo "✅ Task 1/18 finished."
+echo ""
+
+echo "[Task 1/18] Now running: python scripts/train_mujoco.py --env Hopper-v5 --diffusion_steps 1 --alg sac"
+python scripts/train_mujoco.py --env Reacher-v5 --diffusion_steps 20 --alg rf_sac_estient  --noise_scale 0.1 --target_entropy_scale 1.0 --alpha_lr 0.01
+echo "✅ Task 1/18 finished."
+echo ""
+
+# --- 所有任务执行完毕 ---
+echo "=================================================="
+echo "🎉 所有 18 个任务已全部执行完毕！脚本将退出。"
+echo "=================================================="
